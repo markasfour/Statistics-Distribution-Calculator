10081420f1147d59a7f471fea8c7d378f8cd1eab
Mark Asfour <mj4sparrow@gmail.com>
2015-11-11 09:29:06 -0800

    computes E(X), V(X), and sd

--- main.py
+++ main.py
@@ -2,17 +2,51 @@ import os
 import sys
 import math
 
+def isnumber(x):                                    #check if x is a number
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+def nCr(n, r):                                      #n Choose r
+    return math.factorial(n) / math.factorial(r) / math.factorial(n-r)
+
 def binomial(n, p):
     print 'X~B(', n, ', ', p, ')'
+    expected = n * p
+    print 'E(X) = ', expected
+    variance = n * p * (1 - p)
+    print 'V(X) = ', variance
+    sd = math.sqrt(variance)
+    print 'SD(X) = ', sd
 
 def hyper_geometric(n, M, N):
     print 'X~H(', n, ', ', M, ', ', N, ')'
+    expected = n * (M / N) 
+    print 'E(X) = ', expected
+    variance = ((n * M) / N) * (1 - (M / N)) * ((N - n) / (N - 1))
+    print 'V(X) = ', variance
+    sd = math.sqrt(variance)
+    print 'SD(X) = ', sd
 
 def negative_binomial(r, p):
     print 'X~NB(', r, ', ', p, ')'
+    expected = (r * (1 - p)) / p
+    print 'E(X) = ', expected
+    variance = expected / p
+    print 'V(X) = ', variance
+    sd = math.sqrt(variance)
+    print 'SD(X) = ', sd
 
 def poisson(y):
     print 'X~P(', y, ')'
+    expected = y
+    print 'E(X) = ', expected
+    variance = y
+    print 'V(X) = ', variance
+    sd = math.sqrt(variance)
+    print 'SD(X) = ', sd
 
 def main(argv):
     if (argv[0].isalpha()):                         #check if first arg is a letter
@@ -21,24 +55,24 @@ def main(argv):
         if (argv[0] == 'B'):                        #if binomial
             if (len(argv) > 3):
                 print 'Too many arguments'
-            elif (argv[1].isdigit() and argv[2].isdigit()):
-                binomial(argv[1], argv[2])
+            elif (isnumber(argv[1]) and isnumber(argv[2])):
+                binomial(float(argv[1]), float(argv[2]))
             else:
                 print 'Please enter numeric values for n and p'
         
         elif (argv[0] == 'H'):                      #if hyper geometric
             if (len(argv) > 4):
                 print 'Too many arguments'
-            elif (argv[1].isdigit() and argv[2].isdigit() and argv[3].isdigit()):
-                hyper_geometric(argv[1], argv[2], argv[3])
+            elif (isnumber(argv[1]) and isnumber(argv[2]) and isnumber(argv[3])):
+                hyper_geometric(float(argv[1]), float(argv[2]), float(argv[3]))
             else:
                 print 'Please enter numeric values for n, M, and N'
 
         elif (argv[0] == 'NB'):                      #if negative binomial
             if (len(argv) > 3):
                 print 'Too many arguments'
-            elif (argv[1].isdigit() and argv[2].isdigit()):
-                negative_binomial(argv[1], argv[2])
+            elif (isnumber(argv[1]) and isnumber(argv[2])):
+                negative_binomial(float(argv[1]), float(argv[2]))
             else:
                 print 'Please enter numeric values for r and p'
  
@@ -46,8 +80,8 @@ def main(argv):
             print 'P'
             if (len(argv) > 2):
                 print 'Too many arguments'
-            elif (argv[1].isdigit()):
-                poisson(argv[1])
+            elif (isnumber(argv[1])):
+                poisson(float(argv[1]))
             else:
                 print 'Please enter a numeric value for lambda'
  
